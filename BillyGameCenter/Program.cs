77214450f39c2977b657f76a317041cfabc078a9
Bill Chen <billchen@91app.com>
2023-03-25 12:19:56 +0800

    Add Middleware

--- BillyGameCenter/Program.cs
+++ BillyGameCenter/Program.cs
@@ -1,9 +1,21 @@
+using BillyGameCenter;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//builder.Services.AddTransient<ServerRequestMiddleware>();
 
 var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("Before Environment Check Start-----\n");
+    await context.Response.WriteAsync($"Before Environment Check {context.Request} \n");
+    await next();
+    await context.Response.WriteAsync("Before Environment Check End-----\n");
+});
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -16,12 +28,27 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("Before Routing Check Start-----\n");
+    await context.Response.WriteAsync($"Before Routing Check {context.Request}\n");
+    await next();
+    await context.Response.WriteAsync("Before Routing Check End-----\n");
+});
+app.UseMiddleware<ServerRequestMiddleware>();
+
 app.UseRouting();
 
+app.Run(async (context) =>
+{
+    await context.Response.WriteAsync("Just Run Before Authorization \n");
+});
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
---
+++ BillyGameCenter/ServerRequestMiddleware.cs
@@ -0,0 +1,30 @@
+﻿namespace BillyGameCenter
+{
+    public class ServerRequestMiddleware
+    {
+        /// <summary>
+        /// RequestDelegate
+        /// </summary>
+        private readonly RequestDelegate _next;
+
+        /// <summary>
+        /// Initialization 
+        /// </summary>
+        /// <param name="next"></param>
+        public ServerRequestMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        /// <summary>
+        /// Invoke
+        /// </summary>
+        /// <param name="httpContext"></param>
+        public async Task Invoke(HttpContext httpContext)
+        {
+            await httpContext.Response.WriteAsync($"Request：{httpContext.Request} \n");
+            await _next(httpContext);
+            await httpContext.Response.WriteAsync($"Response：{httpContext.Response} \n");
+        }
+    }
+}
